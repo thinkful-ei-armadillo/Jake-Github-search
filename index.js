a8afebd2c034adcee5f56a3eac5d0efd94d40391
JuiceboxJones <jderhalli@gmail.com>
2019-01-26 15:13:28 -0800

    app works, needs css

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 'use strict';
 
-const BASE_URL = 'https://api.github.com';
+const BASE_URL = 'https://api.github.com/users';
+
+function displayResults(responseJson){
+  const searchResults = [];
+  for(let i = 0; i< responseJson.length; i++){
+    searchResults.push(`
+        <li id="results">
+        <p>Name: ${responseJson[i].name}</p>
+        <p>Description: ${responseJson[i].description}</p>
+        <p>Repo URL: <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a></p>
+        </li>`);
+  }
+  $('#search-results').html(searchResults);
+}
+
+
+function fetchResults(url){
+  fetch(url)
+    .then(response =>response.json()
+      .then(responseJson => displayResults(responseJson)))
+    .catch(error => alert('Search results are not working'));
+}
+
+
+function formatSearchURL(users) {
+  const query = users.split(' ').join('&');
+  const completedURL = `${BASE_URL}/${query}/repos`;
+  fetchResults(completedURL);
+  console.log(completedURL);
+}
+
+function watchForm(){
+  $('.search-form').submit(function(event) {
+    event.preventDefault();
+    const users = $('.search-input').val();
+    console.log(users);
+    formatSearchURL(users);
+  });
+}
+
+function main() {
+  watchForm();
+}
+
+$(main);
